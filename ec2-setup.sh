f7cc57be2ebae7041eac7114f8d3bf07f7047999
Kevin Lai <kevinlai31@gmail.com>
2024-07-17 11:59:44 -0500

    setup ec2

--- ec2-setup.sh
+++ ec2-setup.sh
@@ -6,3 +6,4 @@ mkdir -p /usr/local/lib/docker/cli-plugins
 DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -oP '(?<=tag_name": "v)[^"]*')
 curl -SL "https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-linux-x86_64" -o /usr/local/lib/docker/cli-plugins/docker-compose
 chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+chmod 666 /var/run/docker.sock
